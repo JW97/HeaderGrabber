6f6f4770b55bff76fe65e90c6ae45871df8b08b2
Jack Willis <jack.willis@me.com>
2013-01-27 15:56:30 +0000

    Initial Commit

---
+++ HeaderGrabber/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HeaderGrabber
+//
+//  Created by Jack Willis on 27/01/2013.
+//  Copyright (c) 2013 Jack Willis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
